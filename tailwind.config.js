ab32dca1b16daa0508f962756c500205f46ad540
Romanch13 <zloy13081997@gmail.com>
2022-12-28 12:15:02 +0100

    section-3

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,15 @@ module.exports = {
       poppins: ['Poppins', 'sans-serif'],
     },
     screens: {
-      '2xl': {max: '1400px'},
+      '2xl': {min: '1280px', max: '1920px'},
       // => @media (max-width: 1400px) { ... }
-      xl: {max: '1279px'},
+      xl: {min: '992px', max: '1279px'},
       // => @media (max-width: 1279px) { ... }
-      lg: {min: '768px', max: '992px'},
+      lg: {min: '768px', max: '991px'},
       // => @media (min-width: 768px andmax-width: 992px) { ... }
       md: {min: '577px', max: '767px'},
       // => @media (min-width: 577px and max-width: 767px) { ... }
-      sm: {min: '481px', max: '576px'},
+      sm: {min: '480px', max: '576px'},
       // => @media (min-width: 640px and max-width: 767px) { ... }
       xs: {max: '480px'},
       // => @media (max-width: 480px) { ... }
